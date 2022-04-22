a010957f9b4ace7501d617ea665fde28d381b566
The Lafkpages Team <62298468+lafkpages@users.noreply.github.com>
2022-04-22 18:21:24 +0000

    Bash script with threading

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+THREADS="$1"
+
+if [ -z $THREADS ]; then
+  THREADS=2
+fi
+
+export C=0
+
+main () {
+  if [ "$1" = "main" ]; then
+    echo Running $C/$THREADS threads
+    echo
+  fi
+
+  node index.js
+}
+
+for (( i=1; i<=`expr $THREADS - 1`; i++ ))
+do
+  C=`expr $C + 1`
+
+  main > /dev/null &
+done
+
+C=`expr $C + 1`
+main main
